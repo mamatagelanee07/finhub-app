f1c3ca124dc28cee9889e69c62472725d026dbbf
mamatagelanee07 <gelaneem@email.com>
2022-05-06 19:56:04 +0100

    [FIX] Revoke API token commited on GIT by mistake & add secrets gradle plugin to handle secrets

--- network/src/main/java/com/andigeeky/finnhub/network/common/HeaderInterceptor.kt
+++ network/src/main/java/com/andigeeky/finnhub/network/common/HeaderInterceptor.kt
@@ -1,5 +1,6 @@
 package com.andigeeky.finnhub.network.common
 
+import com.andigeeky.finnhub.network.BuildConfig
 import com.andigeeky.finnhub.network.common.NetworkConstants.FINN_HUB_TOKEN_HEADER
 import okhttp3.Interceptor
 import okhttp3.Response
@@ -8,7 +9,7 @@ object HeaderInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
         val requestBuilder = originalRequest.newBuilder()
-                .header(FINN_HUB_TOKEN_HEADER, "c4bs4gqad3idvvrv4du0")
+            .header(FINN_HUB_TOKEN_HEADER, BuildConfig.API_TOKEN)
         val request = requestBuilder.build()
         return chain.proceed(request)
     }
